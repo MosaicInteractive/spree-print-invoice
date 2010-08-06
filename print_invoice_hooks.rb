647f21c29fd4ed53a431143df22ed580fba7aa85
Yves-Eric Martin <yemartin@mediweb.co.jp>
2010-08-06 17:18:17 +0900

    Added printer icon on print invoice buttons (icon from the famfamfam Silk icons collection).

--- print_invoice_hooks.rb
+++ print_invoice_hooks.rb
@@ -1,11 +1,11 @@
 class PrintInvoiceHooks < Spree::ThemeSupport::HookListener
 
   insert_after :admin_order_show_buttons do
-    %( <%= button_link_to(t(:print_invoice, :scope => :print_invoice), formatted_admin_order_url(@order, :pdf)) %>)
+    %( <%= button_link_to(image_tag('admin/icons/printer.png') + " " + t(:print_invoice, :scope => :print_invoice), formatted_admin_order_url(@order, :pdf)) %>)
   end
 
   insert_after :admin_order_edit_buttons do
-    %( <%= button_link_to(t(:print_invoice, :scope => :print_invoice), formatted_admin_order_url(@order, :pdf)) %>)
+    %( <%= button_link_to(image_tag('admin/icons/printer.png') + " " + t(:print_invoice, :scope => :print_invoice), formatted_admin_order_url(@order, :pdf)) %>)
   end
 
 end
